6d74ee8efd70c701136ad6cb1f8de1f877b3d0aa
Zulfaabam <zulfafatahakbar@gmail.com>
2023-03-26 22:08:01 +0700

    [ADD] footer mobile layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import { RiLineLine } from "react-icons/ri";
 import { FiInstagram, FiTwitter, FiPhone, FiMail } from "react-icons/fi";
 import { TfiLocationPin } from "react-icons/tfi";
 
-interface ContactProps {
+export interface ContactProps {
   icon: React.ReactNode;
   link: string;
   content: string;
@@ -22,38 +22,10 @@ export const Footer = () => {
     </div>
   );
 
-  // const contacts = [
-  //   {
-  //     icon: <RiLineLine size={iconSize} />,
-  //     link: "",
-  //     content: "@ntp5446u",
-  //   },
-  //   {
-  //     icon: <FiInstagram size={iconSize} />,
-  //     link: "",
-  //     content: "@gbm_undip",
-  //   },
-  //   {
-  //     icon: <FiTwitter size={iconSize} />,
-  //     link: "",
-  //     content: "@gbm_undip",
-  //   },
-  //   {
-  //     icon: <FiPhone size={iconSize} />,
-  //     link: "",
-  //     content: "+62 838-3873-4981",
-  //   },
-  //   {
-  //     icon: <FiMail size={iconSize} />,
-  //     link: "",
-  //     content: "gbmundip@gmail.com",
-  //   },
-  // ]
-
   return (
-    <footer className="w-full flex justify-between bg-dark px-16 py-7">
-      <div>
-        <div className="flex gap-4">
+    <footer className="w-full flex flex-col gap-5 bg-dark px-16 py-7">
+      <div className="flex justify-center lg:justify-between items-center">
+        <div className="hidden lg:flex gap-4">
           <Image
             src="/images/undip.png"
             alt="Undip Logo"
@@ -79,9 +51,22 @@ export const Footer = () => {
             height={89}
           />
         </div>
+        <div className="flex items-center gap-2">
+          <p className="text-white text-2xl font-heading">
+            Gizi Bakti Masyarakat
+          </p>
+          <Image
+            src="/images/gbm-logo.svg"
+            alt="GBM logo"
+            width={128}
+            height={107}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col-reverse lg:flex-row justify-between items-center">
         <div className="text-white mt-4 flex flex-col gap-2">
-          <p>Narahubung:</p>
-          <div className="flex gap-10">
+          <p className="text-center lg:text-left">Narahubung:</p>
+          <div className="flex flex-col sm:flex-row gap-2 sm:gap-10">
             <div className="flex flex-col gap-2">
               <Contact
                 icon={<RiLineLine size={iconSize} />}
@@ -113,30 +98,23 @@ export const Footer = () => {
             </div>
           </div>
         </div>
-      </div>
-      <div className="flex flex-col items-end gap-3">
-        <div className="flex items-center gap-2">
-          <p className="text-white text-2xl font-heading">
-            Gizi Bakti Masyarakat
+        <div className="flex flex-col gap-2 items-center lg:items-end">
+          <p className="max-w-md text-center lg:text-right text-white">
+            Gizi Bakti Masyarakat (GBM) dibentuk pada tanggal 21 Februari 2008
+            dimana merupakan sebuah mandat dari ILMAGI (Ikatan Lembaga Mahasiswa
+            Gizi Indonesia). Alamat:{" "}
+            <a href="" className="underline lg:hidden">
+              Jalan Prof. H. Soedarto, S.H., Tembalang, Semarang, Jawa Tengah.
+              50275
+            </a>
           </p>
-          <Image
-            src="/images/gbm-logo.svg"
-            alt="GBM logo"
-            width={128}
-            height={107}
-          />
-        </div>
-        <p className="max-w-md text-right text-white">
-          Gizi Bakti Masyarakat (GBM) dibentuk pada tanggal 21 Februari 2008
-          dimana merupakan sebuah mandat dari ILMAGI (Ikatan Lembaga Mahasiswa
-          Gizi Indonesia).
-        </p>
-        <div className="flex items-center gap-2 text-white">
-          <TfiLocationPin size={iconSize} />
-          <a href="" className="underline">
-            Jalan Prof. H. Soedarto, S.H., Tembalang, Semarang, Jawa Tengah.
-            50275
-          </a>
+          <div className="hidden lg:flex items-center gap-2 text-white">
+            <TfiLocationPin size={iconSize} />
+            <a href="" className="underline max-w-md">
+              Jalan Prof. H. Soedarto, S.H., Tembalang, Semarang, Jawa Tengah.
+              50275
+            </a>
+          </div>
         </div>
       </div>
     </footer>

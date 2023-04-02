dec2e85a50a87c40a95fbefd0da7737df3a30667
Zulfaabam <zulfafatahakbar@gmail.com>
2023-04-02 13:57:18 +0700

    [ADD] testimoni card

--- components/layouts/home/Testimoni.tsx
+++ components/layouts/home/Testimoni.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+const TestimoniCard = () => {
+  return (
+    <div className="card card-side bg-base-100 shadow-xl">
+      <figure>
+        <img src="/images/bro.svg" alt="Movie" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">New movie is released!</h2>
+        <p>Click the button to watch on Jetflix app.</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Watch</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Testimoni = () => {
-  return <div>Testimoni</div>;
+  return (
+    <div className="py-7">
+      <div className="max-w-8xl mx-auto">
+        <TestimoniCard />
+      </div>
+    </div>
+  );
 };
 
 export default Testimoni;

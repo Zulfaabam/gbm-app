85e99729d3c981ec533876cb2640ad0764145505
Zulfaabam <zulfafatahakbar@gmail.com>
2023-05-03 15:20:46 +0700

    [ADD] react pdf

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,14 @@
 import Artikel from "components/layouts/home/Artikel";
 import Hero from "components/layouts/home/Hero";
-import InformasiKesehatan from "components/layouts/home/InformasiKesehatan";
 import Layanan from "components/layouts/home/Layanan";
 import Testimoni from "components/layouts/home/Testimoni";
 import MainLayout from "components/layouts/MainLayout";
-import { AuthContext } from "context/AuthContext";
-import { useContext, useEffect } from "react";
 
 export default function Home() {
-  const user = useContext(AuthContext);
-
   return (
     <MainLayout>
       <Hero />
       <Layanan />
-      <InformasiKesehatan />
       <Artikel />
       <Testimoni />
     </MainLayout>
